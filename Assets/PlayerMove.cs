e9f973c56c105855eb05e7ddeb832dfa3dacf1c7
litalico-wonder-akihabara <qremo.akihabara@gmail.com>
2016-11-24 16:29:45 +0900

    first commit

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = transform.position;
+		if (Input.GetKey("up")) {
+			transform.position = new Vector3(pos.x , pos.y , pos.z + 0.1f);
+		}
+		if (Input.GetKey("down")) {
+			transform.position = new Vector3(pos.x , pos.y , pos.z - 0.1f);
+		}
+		if (Input.GetKey("right")) {
+			transform.position = new Vector3(pos.x + 0.1f , pos.y, pos.z );
+
+		}
+		if (Input.GetKey ("left")) {
+			transform.position = new Vector3(pos.x - 0.1f, pos.y, pos.z );
+
+		}
+	}
+}
